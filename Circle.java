7ac9c0b94ccff81f279eccb1c76710f02bdb3f85
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-27 19:29:41 +0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -3,15 +3,21 @@ class Circle extends Shape {
     private double radius;
 
     public Circle(double radius) {
+        
         this.radius = radius;
+        
     }
 
     @Override
+    
     public double calArea() {
+        
         return 3.14 * radius * radius;
+        
     }
 
     @Override
+    
     public boolean equals(Object obj) {
 
         if (obj instanceof Circle) {
@@ -19,8 +25,11 @@ class Circle extends Shape {
             Circle c = (Circle) obj;
 
             return this.radius == c.radius;
+            
         }
 
         return false;
+        
     }
+    
 }
